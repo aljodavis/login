ab1c05e70dd65d02114db83197a531fbb224a17a
aljodavis <aljodavis125@gmail.com>
2024-07-13 09:24:33 +0530

    login  working5

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,15 @@ export default function Home() {
       alt="Picture of the bg"
       className="absolute h-screen w-screen opacity-30 object-cover "
     />
-      <div className="grid w-[90vw] h-[88vh] md:w-[70vw] z-10 lg:w-[50vw] lg:max-w-700 lg:h-[80vh] grid-cols-1">
-        <div className="bg-white text-black flex items-center justify-center flex-col rounded-[6px] dark:bg-black dark:text-white">
+      <div className="grid w-[90vw] h-[90vh] md:w-[78vw] z-10 lg:w-[50vw] lg:max-w-700 lg:h-[80vh] grid-cols-1">
+        <div className="bg-white h-[80vh] md:h-[85vh] lg:h-[76vh] text-black flex items-center justify-center flex-col rounded-[6px] dark:bg-black dark:text-white">
           <div className="my-4">
             <h1 className="text-1.95xl md:text-3xl lg:text-3xl font-bold text-center justify-center">Sign In to Your Account</h1>
             <p className="mt-2 p-1 text-xs lg:text-xs  text-center justify-center text-slate-400 mx-auto w-4/5">Welcome to a smarter way of managing tasks and products.Our comprehensive suit is designed to streamline your workflow,enhance collaboration.</p>
 
           </div>
-          <form className="w-4/5 h-full  md:w-3/4 md:h-2/3 lg:h-3/4">
-            <Button className="flex items-center w-full mt-2 px-4 md:px-12 bg-transparet rounded-[6px] gap-1 dark:text-white" variant="outline">
+          <form className="w-4/5 h-4/5  md:w-5/6 md:h-2/3 lg:h-3/4">
+            <Button className=" sm:p-2 flex items-center w-full mt-2 px-4 md:px-12 bg-transparet rounded-[6px] gap-1 dark:text-white" variant="outline">
               <FcGoogle />
               Sign in with Google
             </Button>
@@ -49,7 +49,7 @@ export default function Home() {
 
             <div className="mt-0.5 mb-1 text-xs lg:mb-3 text-right w-full dark:text-white">Forgot your password?</div>
 
-            <Button type="submit" className="w-full mt-6 bg-blue-600 text-white rounded-[6px] hover:bg-blue-900">
+            <Button type="submit" className="w-full mt-6 md:mt-5 lg:mt-7 bg-blue-600 text-white rounded-[6px] hover:bg-blue-900">
               Sign in
             </Button>
           </form>
